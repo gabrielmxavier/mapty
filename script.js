369accf6ddb8f71b63d5d5e70d91bf890b39fe6e
Gabriel Xavier <51133838+gabrielmxavier@users.noreply.github.com>
2021-03-08 22:00:53 -0300

    simple update (#7)

--- script.js
+++ script.js
@@ -10,3 +10,66 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+
+        //console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+
+        map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // handling clicks on map
+        map.on('click', function (mapEvents) {
+            mapEvent = mapEvents;
+            form.classList.remove('hidden');
+            inputDistance.focus()
+
+
+        })
+
+    },
+        function () {
+            alert('You need to give a permition to get the current position!')
+        })
+};
+
+form.addEventListener('submit', function (e) {
+    // Display the marker
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+
+
+    console.log(mapEvent);
+
+    const { lat, lng } = mapEvent.latlng
+
+    L.marker([lat, lng], { riseOnHover: true })
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+        .setPopupContent(`Latitude: ${lat}<br>Longitude: ${lng}`)
+        .openPopup();
+});
+
+inputType.addEventListener('change', function() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
