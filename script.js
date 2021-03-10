18e44455338ae75b1016cf1ab43eb0e69a495c27
gabrielmxavier <gabrielxavierbonano@gmail.com>
2021-03-10 14:29:59 -0300

    refactored

--- script.js
+++ script.js
@@ -10,3 +10,77 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+    #map;
+    #mapEvent;
+
+    constructor() {
+        this._getPosition();
+
+        form.addEventListener('submit', this._newWorkout.bind(this));
+
+        inputType.addEventListener('change', this._toggleElevationField);
+
+    }
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert('You need to give a permition to get the current position!')
+            })
+        };
+    }
+
+    _loadMap(position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+
+        //console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+        // handling clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapEvents) {
+        this.#mapEvent = mapEvents;
+        form.classList.remove('hidden');
+        inputDistance.focus()
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+
+    _newWorkout(e) {
+        // Display the marker
+        e.preventDefault();
+
+        // Clear input fields
+        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+
+        const { lat, lng } = this.#mapEvent.latlng
+
+        L.marker([lat, lng], { riseOnHover: true })
+            .addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent(`Latitude: ${lat}<br>Longitude: ${lng}`)
+            .openPopup();
+    }
+}
+
+const app = new App();
